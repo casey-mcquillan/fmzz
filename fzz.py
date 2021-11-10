7b990a1869006e9c3776a89acc648bcb760dbeb3
Casey McQuillan <mcquillan.casey@gmail.com>
2021-11-10 11:51:54 -0500

    Adjust for new calibration process
    Adjust for new calibration process that uses group LFP instead of epop ratio

--- fzz.py
+++ fzz.py
@@ -25,45 +25,46 @@ os.chdir(data_folder)
 df_observed = pd.read_csv('observed_data.csv', index_col=0)
 
 
-#%%  Calibration Over Time #%% 
+#%%  Baseline Case #%% 
 # Parameter assumptions:
 alpha_c=1
 alpha_n=1
 
 # Outcomes to track
-years = [1977,1987] + list(range(1996, 2019))
-results_variables = ['Percent Reduction in College Wage Premium']
-df_results = pd.DataFrame(columns=results_variables, index=years)
+year = 2018
 
-# Loop through and calibrate model each year
-for year in years:
-    
-    #Define and calibrate model
-    model = calibration_model(alpha_c, alpha_n,
-                tau=df_observed.loc[year, 'tau_high'], 
-                w1_c=df_observed.loc[year, 'wage_c'], 
-                w1_n=df_observed.loc[year, 'wage_n'],
-                share_workers1_c=df_observed.loc[year, 'share_workers1_c'],
-                share_pop_c=df_observed.loc[year, 'share_pop_c'],
-                epop_ratio1=df_observed.loc[year, 'epop_ratio'],
-                pop_count=df_observed.loc[year, 'pop_count'])
-    
-    #Make sure there are no NANs in model before calibration
-    if any(np.isnan([vars(model)[x] for x in vars(model).keys()])):
-        print("NAN value entered into calibration model for:")
-        for var in vars(model).keys():
-            if np.isnan(vars(model)[var])==True: print("    "+var)
-        print("for year: " + str(year))
-        continue
-    
-    #Calibrate Model
-    model.calibrate()
-    
-    #Collect relevant statistics
-    college_wage_premium1 = (model.w1_c - model.w1_n)
-    college_wage_premium2 = (model.w2_c - model.w2_n)
-    y = (college_wage_premium2 - college_wage_premium1) / college_wage_premium1
-    df_results.loc[year, 'Percent Reduction in College Wage Premium'] = y
+#Define and calibrate model
+model = calibration_model(alpha_c, alpha_n,
+            tau=df_observed.loc[year, 'tau_high'], 
+            w1_c=df_observed.loc[year, 'wage1_c'], 
+            w1_n=df_observed.loc[year, 'wage1_n'],
+            P1_c=df_observed.loc[year, 'P1_c'], 
+            P1_n=df_observed.loc[year, 'P1_n'],
+            share_workers1_c=df_observed.loc[year, 'share_workers1_c'],
+            share_pop_c=df_observed.loc[year, 'share_pop_c'],
+            pop_count=df_observed.loc[year, 'pop_count'])
+
+#Make sure there are no NANs in model before calibration
+if any(np.isnan([vars(model)[x] for x in vars(model).keys()])):
+    print("NAN value entered into calibration model for:")
+    for var in vars(model).keys():
+        if np.isnan(vars(model)[var])==True: print("    "+var)
+    print("for year: " + str(year))
+
+#Calibrate Model
+model.calibrate()
+
+#Output LaTeX Tables
+output_path = '/Users/caseymcquillan/Desktop/Research/FZZ/output/Tables/Baseline'
+model.generate_table(file_name='SummaryTable'+str(year)+"_baseline", year=year, 
+                     table_type="equilibrium summary", 
+                     table_label="SummaryTable"+str(year)+"_baseline", 
+                     location=output_path)
+
+model.generate_table(file_name='EqComparison'+str(year)+"_baseline", year=year, 
+                 table_type="equilibrium comparison", 
+                 table_label="EqComparison"+str(year)+"_baseline", 
+                 location=output_path)
     
     
 #%%  Summary Table Over Time #%% 
@@ -72,22 +73,26 @@ alpha_c=1
 alpha_n=1
 
 #Output path and define years
+
 output_path = '/Users/caseymcquillan/Desktop/Research/FZZ/output/Tables'
 years = [1977,1987] + list(range(1996, 2019))
+P1_c= pd.DataFrame(index=years)
+P1_n= pd.DataFrame(index=years)
 for year in years:
     
     #Define and calibrate model
     model = calibration_model(alpha_c, alpha_n,
                 tau=df_observed.loc[year, 'tau_high'], 
-                w1_c=df_observed.loc[year, 'wage_c'], 
-                w1_n=df_observed.loc[year, 'wage_n'],
+                w1_c=df_observed.loc[year, 'wage1_c'], 
+                w1_n=df_observed.loc[year, 'wage1_n'],
+                P1_c=df_observed.loc[year, 'P1_c'], 
+                P1_n=df_observed.loc[year, 'P1_n'],
                 share_workers1_c=df_observed.loc[year, 'share_workers1_c'],
                 share_pop_c=df_observed.loc[year, 'share_pop_c'],
-                epop_ratio1=df_observed.loc[year, 'epop_ratio'],
                 pop_count=df_observed.loc[year, 'pop_count'])
 
 
-        #Make sure there are no NANs in model before calibration
+    #Make sure there are no NANs in model before calibration
     if any(np.isnan([vars(model)[x] for x in vars(model).keys()])):
         print("NAN value entered into calibration model for:")
         for var in vars(model).keys():
@@ -107,6 +112,3 @@ for year in years:
     model.generate_table(file_name='EqComparison'+str(year), year=year, 
                      table_type="equilibrium comparison", table_label="EqComparison"+str(year), 
                      location=output_path+'/SummaryTable_byYear')
-    
-    
-    
