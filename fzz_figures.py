dd2df20607260d174578ca530662c19a8e0b1639
Casey McQuillan <mcquillan.casey@gmail.com>
2021-11-10 11:53:15 -0500

    Create fzz_figures.py

---
+++ fzz_figures.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 10 11:28:36 2021
+
+@author: caseymcquillan
+"""
+#%%  Preamble: Import packages, set directory #%%  
+### Import Packages
+import os
+import pandas as pd
+import numpy as np
+
+### Set working directory
+code_folder = "/Users/caseymcquillan/Desktop/Research/FZZ/code"
+data_folder = "/Users/caseymcquillan/Desktop/Research/FZZ/data"
+os.chdir(code_folder)
+
+### Import calibration class
+from fzz_calibration import calibration_model 
+
+
+#%%  Importing Data #%%  
+os.chdir(data_folder)
+df_observed = pd.read_csv('observed_data.csv', index_col=0)
+
+
+#%%  Summary Table Over Time #%% 
+# Parameter assumptions:
+alpha_c=1
+alpha_n=1
+
+#Output path and define years
+
+output_path = '/Users/caseymcquillan/Desktop/Research/FZZ/output/Tables'
+years = [1977,1987] + list(range(1996, 2019))
+P1_c= pd.DataFrame(index=years)
+P1_n= pd.DataFrame(index=years)
+for year in years:
+    
+    #Define and calibrate model
+    model = calibration_model(alpha_c, alpha_n,
+                tau=df_observed.loc[year, 'tau_high'], 
+                w1_c=df_observed.loc[year, 'wage1_c'], 
+                w1_n=df_observed.loc[year, 'wage1_n'],
+                P1_c=df_observed.loc[year, 'P1_c'], 
+                P1_n=df_observed.loc[year, 'P1_n'],
+                share_workers1_c=df_observed.loc[year, 'share_workers1_c'],
+                share_pop_c=df_observed.loc[year, 'share_pop_c'],
+                pop_count=df_observed.loc[year, 'pop_count'])
+
+
+    #Make sure there are no NANs in model before calibration
+    if any(np.isnan([vars(model)[x] for x in vars(model).keys()])):
+        print("NAN value entered into calibration model for:")
+        for var in vars(model).keys():
+            if np.isnan(vars(model)[var])==True: print("    "+var)
+        print("for year: " + str(year))
+        continue
+    
+    #Calibrate Model
+    model.calibrate()
