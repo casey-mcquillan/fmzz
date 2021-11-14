356b4aa63a9a0de1b328948766a46b653f36dc13
Casey McQuillan <mcquillan.casey@gmail.com>
2021-11-13 21:04:17 -0500

    Combining sections of ASEC_data_clean

--- ASEC_data_clean.py
+++ ASEC_data_clean.py
@@ -19,92 +19,87 @@ os.chdir(code_folder)
 #%% Import Data #%%
 os.chdir(data_folder)
 df = pd.read_csv('cps_00009.csv')
+
+
+#%% Data Wrangling #%%
+#Drop invalid responses
+for var in ['EDUC', 'WKSWORK2', 'UHRSWORKLY', 'CLASSWLY', 'INCWAGE']:
+    df = df[[not np.isnan(x) for x in df[var]]]
+df = df[[not x in [0,1,999] for x in df['EDUC']]]
+df = df[df['WKSWORK2']!=9]
+df = df[df['CLASSWLY']!=99]
+
+#Adjust year bc survey data corresponds to prev year
 df['YEAR'] = df['YEAR'] - 1
 
-#%% Create Share of Population with College Education #%%
-df1 = df
-
-#Drop invalid responses
-df1 = df1[[not np.isnan(x) for x in df1['EDUC']]]
-df1 = df1[[not x in [0,1,999] for x in df1['EDUC']]]
-
-# Create this variable to track number of observations after collapse
-df1['N'] = 1
-
 # Define college attendance
-df1['college'] = \
-    [int(x in [110, 120, 121, 122, 111, 123, 124, 125]) for x in df1['EDUC']]
-df1['college (weighted)'] = \
-    df1['college'] * df1['ASECWT']
+df['college'] = \
+    [int(x in [110, 120, 121, 122, 111, 123, 124, 125]) for x in df['EDUC']]
+df['college (weighted)'] = \
+    df['college'] * df['ASECWT']
 
-#Create collapsed data frame
-data = df1.groupby('YEAR').sum()  
-   
-#Calculate share educated 
-data['share_pop_c'] = \
-    data['college'] / data['N']
-data['share_pop_c (weighted)'] = \
-    data['college (weighted)'] / data['ASECWT']
+# Define working
+hours_requirment =  1*(df['UHRSWORKLY'] >= 35)
+weeks_requirment =  1*(df['WKSWORK2'] >= 4)
+worker_class_requirement = [int(not x in [13,14,29]) for x in df['CLASSWLY']]
 
-## Store data
-share_pop_c = data[['share_pop_c', 'share_pop_c (weighted)']]
+df['working'] = hours_requirment * weeks_requirment * worker_class_requirement
+df['working (weighted)'] = hours_requirment * weeks_requirment* df['ASECWT']
+
+# Collect wage data
+df['wage_college'] = df['working'] * df['college'] * df['INCWAGE']
+df['wage_noncollege'] = df['working'] * (1 - df['college']) * df['INCWAGE']
+
+df['wage_college (weighted)'] = df['working'] * df['college'] * df['INCWAGE'] * df['ASECWT'] 
+df['wage_noncollege (weighted)'] = df['working'] * (1 - df['college']) * df['INCWAGE'] * df['ASECWT']
 
 
-#%% Create Export for Share of Workforce with College Education #%%
-df2 = df
-
-# Drop nan observations
-for var in ['EDUC', 'WKSWORK2', 'UHRSWORKLY', 'CLASSWLY', 'INCWAGE']:
-    df2 = df2[[not np.isnan(x) for x in df2[var]]]
-    
-# Drop other invalid responses
-df2 = df2[[not x in [0,1,999] for x in df2['EDUC']]]
-df2 = df2[[not x in [0,9] for x in df2['WKSWORK2']]]
-df2 = df2[df2['UHRSWORKLY']!=999]
-df2 = df2[df2['CLASSWLY']!=99]
-
-# Drop those who didn't work full-time, full-year
-df2 = df2[df2['UHRSWORKLY'] >= 35]
-df2 = df2[df2['WKSWORK2'] >= 4]
-df2 = df2[[not x in [13,14,29] for x in df2['CLASSWLY']]]
-
-# Define college attendance
-df2['college'] = \
-    [int(x in [110, 120, 121, 122, 111, 123, 124, 125]) for x in df2['EDUC']]
-df2['college (weighted)'] = \
-    df2['college'] * df2['ASECWT']
-    
+#%% Data Collapse #%%
 # Create this variable to track number of observations after collapse
-df2['N'] = 1
+df['N'] = 1
+df['N_college'] = df['college']
+df['N_noncollege'] = 1 - df['college']
+df['N_working'] = df['working']
+df['N_college_working'] = df['college'] * df['working']
+df['N_noncollege_working'] = (1-df['college']) * df['working']
 
-# Create these variables to track average wages after collapse
-df2['wage_college'] = df2['college'] * df2['INCWAGE']
-df2['wage_noncollege'] = (1 - df2['college']) * df2['INCWAGE']
+# Create weights 
+df['ASECWT']
+df['ASECWT_college'] = df['N_college'] * df['ASECWT']
+df['ASECWT_noncollege'] = df['N_noncollege'] * df['ASECWT']
+df['ASECWT_working'] = df['N_working'] * df['ASECWT']
+df['ASECWT_college_working'] = df['N_college_working'] * df['ASECWT']
+df['ASECWT_noncollege_working'] = df['N_noncollege_working'] * df['ASECWT']
 
-df2['wage_college (weighted)'] = df2['college'] * df2['INCWAGE'] * df2['ASECWT'] 
-df2['wage_noncollege (weighted)'] = (1 - df2['college']) * df2['INCWAGE'] * df2['ASECWT']
-df2['ASECWT_college'] = df2['college'] * df2['ASECWT']
-df2['ASECWT_noncollege'] = (1 - df2['college']) * df2['ASECWT']
+# Collapse data by year
+data = df.groupby('YEAR').sum()  
 
-#Create collapsed data frame
-data = df2.groupby('YEAR').sum()  
-   
-#Calculate share educated 
-data['share_workers1_c'] = \
-    data['college'] / data['N']
-data['share_workers1_c (weighted)'] = \
-    data['college (weighted)'] / data['ASECWT']
-    
+# Calculate variables
+data['P1_c'] = data['N_college_working'] / data['N_college']
+data['P1_c (weighted)'] = data['ASECWT_college_working'] / data['ASECWT_college']
+
+data['P1_n'] = data['N_noncollege_working'] / data['N_noncollege']
+data['P1_n (weighted)'] = data['ASECWT_noncollege_working'] / data['ASECWT_noncollege']
+
+data['share_workers1_c'] = data['N_college_working'] / data['N_working']
+data['share_workers1_c (weighted)'] = data['ASECWT_college_working'] / data['ASECWT_working']
+
+data['share_pop_c'] = data['N_college'] / data['N']
+data['share_pop_c (weighted)'] = data['ASECWT_college'] / data['ASECWT']
+
 #Calculate wages
 data['wage1_c'] = \
-    data['wage_college'] / data['college'] 
+    data['wage_college'] / data['N_college_working'] 
 data['wage1_n'] = \
-    data['wage_noncollege'] / (data['N'] - data['college'])
-data['wage1_c (weighted)'] = \
-    data['wage_college (weighted)'] / data['ASECWT_college']
-data['wage1_n (weighted)'] = \
-    data['wage_noncollege (weighted)'] / data['ASECWT_noncollege']
+    data['wage_noncollege'] / data['N_noncollege_working']
 
+data['wage1_c (weighted)'] = \
+    data['wage_college (weighted)'] / data['ASECWT_college_working']
+data['wage1_n (weighted)'] = \
+    data['wage_noncollege (weighted)'] / data['ASECWT_noncollege_working']
+    
+    
+#%% Inflation Adjust #%%
 # Adjust wages to be in 2019 dollars
 os.chdir(data_folder)
 price_data = pd.read_csv('PCEPI_data.csv', index_col=0)
@@ -112,63 +107,15 @@ for year in data.index:
     adj_factor = price_data.loc[year, 'PCEPI Adjustment Factor (2019 Dollars)']
     for var in ['wage1_c', 'wage1_n', 'wage1_c (weighted)', 'wage1_n (weighted)']:
         data.loc[year, var] = adj_factor*data.loc[year, var]
-        
-## Store data
-share_workers1_c = data[['share_workers1_c', 'share_workers1_c (weighted)']]
-wage = data[['wage1_c', 'wage1_n', 'wage1_c (weighted)', 'wage1_n (weighted)']]
-
-
-#%% Calculate share of each group that chooses to work #%%
-df3 = df
-
-#Drop invalid responses
-for var in ['EDUC', 'WKSWORK2', 'UHRSWORKLY', 'CLASSWLY', 'INCWAGE']:
-    df3 = df3[[not np.isnan(x) for x in df3[var]]]
-df3 = df3[[not x in [0,1,999] for x in df3['EDUC']]]
-df3 = df3[df3['WKSWORK2']!=9]
-df3 = df3[df3['CLASSWLY']!=99]
-
-# Define college attendance
-df3['college'] = \
-    [int(x in [110, 120, 121, 122, 111, 123, 124, 125]) for x in df3['EDUC']]
-df3['college (weighted)'] = \
-    df3['college'] * df3['ASECWT']
-
-# Define working
-hours_requirment =  1*(df3['UHRSWORKLY'] >= 35)
-weeks_requirment =  1*(df3['WKSWORK2'] >= 4)
-worker_class_requirement = [int(not x in [13,14,29]) for x in df3['CLASSWLY']]
-
-df3['working'] = hours_requirment * weeks_requirment * worker_class_requirement
-df3['working (weighted)'] = hours_requirment * weeks_requirment* df3['ASECWT']
-
-# Create this variable to track number of observations after collapse
-df3['N'] = 1
-
-# Separate based on education
-df3_c = df3[df3['college'] == 1]
-df3_n = df3[df3['college'] == 0]
-
-#Create collapsed data frame
-data_c = df3_c.groupby('YEAR').sum()
-data_n = df3_n.groupby('YEAR').sum()
-   
-#Calculate share educated 
-data_c['P1_c'] = \
-    data_c['working'] / data_c['N']
-data_c['P1_c (weighted)'] = \
-    data_c['working (weighted)'] / data_c['ASECWT']
-    
-data_n['P1_n'] = \
-    data_n['working'] / data_n['N']
-data_n['P1_n (weighted)'] = \
-    data_n['working (weighted)'] / data_n['ASECWT']
-
-## Store data
-LFP_shares = pd.concat([data_c[['P1_c', 'P1_c (weighted)']], data_n[['P1_n','P1_n (weighted)']]], axis=1)
 
 
 #%% Export Data #%%
 os.chdir(data_folder)
-data_export = pd.concat([share_pop_c, share_workers1_c, wage,LFP_shares], axis=1)
-data_export.to_csv('CPS_ASEC_clean.csv')
+data_export = data[['N', 'N_college', 'N_working', 'N_college_working',
+                    'share_pop_c', 'share_pop_c (weighted)', 
+                    'share_workers1_c', 'share_workers1_c (weighted)',
+                    'P1_c', 'P1_c (weighted)',
+                    'P1_n', 'P1_n (weighted)',
+                    'wage1_c', 'wage1_c (weighted)',
+                    'wage1_n', 'wage1_n (weighted)']]
+data_export.to_csv('CPS_ASEC_clean.csv')    
--- fzz_figures.py
+++ fzz_figures.py
@@ -14,6 +14,7 @@ import numpy as np
 ### Set working directory
 code_folder = "/Users/caseymcquillan/Desktop/Research/FZZ/code"
 data_folder = "/Users/caseymcquillan/Desktop/Research/FZZ/data"
+output_folder = "/Users/caseymcquillan/Desktop/Research/FZZ/output/Graphs/Figures"
 os.chdir(code_folder)
 
 ### Import calibration class
@@ -24,6 +25,10 @@ from fzz_calibration import calibration_model
 os.chdir(data_folder)
 df_observed = pd.read_csv('observed_data.csv', index_col=0)
 
+# Import time series data on wages, tau from Patrick Collard:
+os.chdir(data_folder + "/Time Series from Patrick")
+premium_data = pd.read_excel('premium_series.xlsx', index_col=0)
+
 
 #%%  Summary Table Over Time #%% 
 # Parameter assumptions:
@@ -31,11 +36,19 @@ alpha_c=1
 alpha_n=1
 
 #Output path and define years
-
-output_path = '/Users/caseymcquillan/Desktop/Research/FZZ/output/Tables'
 years = [1977,1987] + list(range(1996, 2019))
-P1_c= pd.DataFrame(index=years)
-P1_n= pd.DataFrame(index=years)
+
+
+outcome_variables = ['wage_premium1', 'wage_premium2',
+             'wage_share1', 'wage_share2', 
+             'share_workers1_c', 'share_workers2_c',
+             'P1_c', 'P1_n', 'P2_c', 'P2_n',
+             'w1_c', 'w1_n', 'w2_c', 'w2_n',
+             'reduction_college_wage_premium',
+             'reduction_college_wage_share']
+
+df_results = pd.DataFrame(index=years, columns=outcome_variables)
+
 for year in years:
     
     #Define and calibrate model
@@ -60,3 +73,203 @@ for year in years:
     
     #Calibrate Model
     model.calibrate()
+    
+ 
+    df_results.loc[year,'wage_premium1'] = model.w1_c - model.w1_n
+    df_results.loc[year,'wage_premium2'] = model.w2_c - model.w2_n
+    df_results.loc[year,'wage_share1'] = model.w1_c * model.L1_c  \
+                                            / (model.w1_c * model.L1_c + model.w1_n * model.L1_n)
+    df_results.loc[year,'wage_share2'] = model.w2_c * model.L2_c  \
+                                            / (model.w2_c * model.L2_c + model.w2_n * model.L2_n)
+    df_results.loc[year,'share_workers1_c'] = model.share_workers1_c
+    df_results.loc[year,'share_workers2_c'] = model.share_workers2_c
+    df_results.loc[year,'P1_c'] = model.P1_c
+    df_results.loc[year,'P1_n'] = model.P1_n
+    df_results.loc[year,'P2_c'] = model.P2_c
+    df_results.loc[year,'P2_n'] = model.P2_n
+    df_results.loc[year,'w1_c'] = model.w1_c
+    df_results.loc[year,'w1_n'] = model.w1_n
+    df_results.loc[year,'w2_c'] = model.w2_c
+    df_results.loc[year,'w2_n'] = model.w2_n
+    df_results.loc[year,'reduction_college_wage_premium'] = \
+        -100*((model.w2_c - model.w2_n) - (model.w1_c - model.w1_n))/(model.w1_c - model.w1_n)
+    df_results.loc[year,'reduction_college_wage_share'] = \
+        -100*((df_results.loc[year,'wage_share2']) - (df_results.loc[year,'wage_share1']))
+    
+    
+#%%  Graphs #%% 
+# Import necessary Packages
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("darkgrid")
+
+
+# Change path to output folder
+os.chdir(output_folder)
+
+## Graphs ###
+#  Share of population with a college degree
+plt.plot(df_observed['share_pop_c'])
+plt.ylim([0,0.5])
+plt.title("Share of Population with Bachelor's Degree or More", fontsize=14)
+plt.savefig('share_pop_c.png', dpi=500)
+plt.clf() 
+
+
+#  Share of workeforce with a college degree
+plt.plot(df_observed['share_workers1_c'])
+plt.ylim([0,0.5])
+plt.title("Share of Workers with Bachelor's Degree or More", fontsize=14)
+plt.savefig('share_workers_c_observed.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['share_workers1_c'], label="Head Tax Equilibrium")
+plt.plot(df_results['share_workers2_c'], label="Payroll Tax Equilibrium")
+plt.legend()
+plt.ylim([0,0.5])
+plt.title("Share of Workers with Bachelor's Degree or More", fontsize=14)
+plt.savefig('share_workers_c.png', dpi=500)
+plt.clf() 
+
+
+#  Both shares
+plt.plot(df_observed['share_workers1_c'], label="Share of Workforce with College Degree")
+plt.plot(df_observed['share_pop_c'], label="Share of Population with College Degree")
+plt.legend()
+plt.ylim([0,0.5])
+plt.savefig('share_college.png', dpi=500)
+plt.clf() 
+
+#  Employment Rate
+plt.plot(df_observed['P1_c'], label="College", color='darkred')
+plt.plot(df_observed['P1_n'], label="Non-college", color='navy')
+plt.legend()
+plt.ylim([0.5,0.8])
+plt.title("Labor Force Participation by Group", fontsize=14)
+plt.savefig('LFP_observed.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['P1_c'], label="Head Tax Equilibrium", color='darkred')
+plt.plot(df_results['P2_c'], label="Payroll Tax Equilibrium", color='indianred')
+plt.legend()
+plt.ylim([0.65,0.8])
+plt.title("Labor Force Participation for College Workers", fontsize=14)
+plt.savefig('LFP_c.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['P1_n'], label="Head Tax Equilibrium", color='navy')
+plt.plot(df_results['P2_n'], label="Payroll Tax Equilibrium", color='steelblue')
+plt.legend()
+plt.ylim([0.5,0.70])
+plt.title("Labor Force Participation for Non-college Workers", fontsize=14)
+plt.savefig('LFP_n.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['P1_c'], label="College, Head Tax", color='darkred')
+plt.plot(df_results['P2_c'], label="College, Payroll Tax", color='indianred')
+plt.plot(df_results['P1_n'], label="Non-college, Head Tax", color='navy')
+plt.plot(df_results['P2_n'], label="Non-college, Payroll Tax", color='steelblue')
+plt.legend()
+plt.ylim([0.5,0.8])
+plt.title("Labor Force Participation by Group", fontsize=14)
+plt.savefig('LFP.png', dpi=500)
+plt.clf() 
+
+
+#  Wages
+plt.plot(df_observed['wage1_c'], label="College", color='darkred')
+plt.plot(df_observed['wage1_n'], label="Non-college", color='navy')
+plt.legend()
+plt.title("Wages by Group", fontsize=14)
+plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.StrMethodFormatter('{x:,.0f}'))
+plt.savefig('wages_observed.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['w1_c'], label="Head Tax Equilibrium", color='darkred')
+plt.plot(df_results['w2_c'], label="Payroll Tax Equilibrium", color='indianred')
+plt.legend()
+plt.ylim([60000,100000])
+plt.title("Wages for College Workers", fontsize=14)
+plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.StrMethodFormatter('{x:,.0f}'))
+plt.savefig('wages_c.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['w1_n'], label="Head Tax Equilibrium", color='navy')
+plt.plot(df_results['w2_n'], label="Payroll Tax Equilibrium", color='steelblue')
+plt.legend()
+plt.ylim([40000,55000])
+plt.title("Wages for Non-college Workers", fontsize=14)
+plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.StrMethodFormatter('{x:,.0f}'))
+plt.savefig('wages_n.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['w1_c'], label="College, Head Tax", color='darkred')
+plt.plot(df_results['w2_c'], label="College, Payroll Tax", color='indianred')
+plt.plot(df_results['w1_n'], label="Non-college, Head Tax", color='navy')
+plt.plot(df_results['w2_n'], label="Non-college, Payroll Tax", color='steelblue')
+plt.legend()
+plt.ylim([40000,100000])
+plt.title("Wages by Group", fontsize=14)
+plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.StrMethodFormatter('{x:,.0f}'))
+plt.savefig('wages.png', dpi=500)
+plt.clf() 
+
+
+#  College wage premium
+plt.plot(df_observed['wage1_c']-df_observed['wage1_n'])
+plt.title("College Wage Premium", fontsize=14)
+plt.savefig('college_wage_premium_observed.png', dpi=500)
+plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.StrMethodFormatter('{x:,.0f}'))
+plt.savefig('college_wage_premium_observed.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['wage_premium1'], label="Head Tax Equilibrium")
+plt.plot(df_results['wage_premium2'], label="Payroll Tax Equilibrium")
+plt.legend()
+plt.title("College Wage Premium", fontsize=14)
+plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.StrMethodFormatter('{x:,.0f}'))
+plt.savefig('college_wage_premium.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['reduction_college_wage_premium'])
+plt.suptitle("Pct. Reduction in College Wage Premium", y=0.99, fontsize=14)
+plt.title(r'(Head Tax $\Rightarrow$ Payroll tax)', fontsize=12)
+plt.savefig('reduction_college_wage_premium.png', dpi=500)
+plt.clf() 
+
+
+#  College share
+plt.plot(df_results['wage_share1'])
+plt.title("College Share of Total Wage Bill", fontsize=14)
+plt.savefig('wage_share_observed.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['wage_share1'], label="Head Tax Equilibrium")
+plt.plot(df_results['wage_share2'], label="Payroll Tax Equilibrium")
+plt.title("College Share of Total Wage Bill", fontsize=14)
+plt.legend()
+plt.savefig('wage_share.png', dpi=500)
+plt.clf() 
+
+plt.plot(df_results['reduction_college_wage_share'])
+plt.suptitle('PP. Reduction in College Share of Total Wage Bill', y=0.99, fontsize=14)
+plt.title(r'(Head Tax $\Rightarrow$ Payroll tax)', fontsize=12)
+plt.savefig('reduction_college_wage_share.png', dpi=500)
+plt.clf() 
+
+
+#  Tau parameters
+plt.plot(df_observed['tau_high'].dropna(), label='Total Cost and Complete Take-up')
+plt.plot(df_observed['tau_med'].dropna(), label='Cost to Employer and Complete Take-up')
+plt.plot(df_observed['tau_low'].dropna(), label='Cost to Employer and Incomplete Take-up')
+plt.legend()
+plt.title("Cost of ESHI", fontsize=14)
+plt.gca().yaxis.set_major_formatter(plt.matplotlib.ticker.StrMethodFormatter('{x:,.0f}'))
+plt.savefig('tau.png', dpi=500)
+plt.clf() 
+
+
+
+
+
