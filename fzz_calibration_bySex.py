ec026a52e64c2ee7165a28cea8e98730e91f07e4
Casey McQuillan <mcquillan.casey@gmail.com>
2022-07-12 11:31:16 -0400

    Update fzz_calibration_bySex.py

--- fzz_calibration_bySex.py
+++ fzz_calibration_bySex.py
@@ -18,10 +18,11 @@ code_folder = main_folder+"/code"
 data_folder = main_folder+"/data"
 os.chdir(code_folder)
 
+
 #%%  Define Class #%%
 class calibration_model_bySex:
     def __init__(self, alpha_diff, rho, tau, 
-                     elasticity_c, elasticity_n,
+                     elasticities,
                      w1_c_m, w1_n_m, w1_c_f, w1_n_f, 
                      P1_c_m, P1_n_m, P1_c_f, P1_n_f,
                      share_workers1_c, share_pop_c,
@@ -33,8 +34,7 @@ class calibration_model_bySex:
         self.alpha_diff = alpha_diff
         self.rho = rho
         self.tau = tau
-        self.elasticity_c = elasticity_c
-        self.elasticity_n = elasticity_n
+        self.elasticities = elasticities
         
         #Wages
         self.w1_c_m = w1_c_m
@@ -74,8 +74,7 @@ class calibration_model_bySex:
         alpha_diff = self.alpha_diff
         rho = self.rho
         tau = self.tau
-        elasticity_c = self.elasticity_c
-        elasticity_n = self.elasticity_n
+        elasticities = self.elasticities
         
         #Wages
         w1_c_m = self.w1_c_m
@@ -90,12 +89,16 @@ class calibration_model_bySex:
         P1_n_f = self.P1_n_f
         
         #Shares
+        share_workers1_c = self.share_workers1_c
         share_workers1_c_m = self.share_workers1_c_m
         share_workers1_c_f = self.share_workers1_c_f
+        share_workers1_n = self.share_workers1_n
         share_workers1_n_m = self.share_workers1_n_m
         share_workers1_n_f = self.share_workers1_n_f
+        share_pop_c = self.share_pop_c
         share_pop_c_m = self.share_pop_c_m
         share_pop_c_f = self.share_pop_c_f
+        share_pop_n = self.share_pop_n
         share_pop_n_m = self.share_pop_n_m
         share_pop_n_f = self.share_pop_n_f
         pop_count = self.pop_count
@@ -120,7 +123,7 @@ class calibration_model_bySex:
         lambda_n = coef*lambda_c
         
         # Solve for kappa parameters
-        if elasticity_c==elasticity_c=='common':
+        if elasticities =='common':
             kappa_dist_m = (w1_c_m - w1_n_m + alpha_diff*tau)/(P1_c_m - P1_n_m)
             kappa_dist_f = (w1_c_f - w1_n_f + alpha_diff*tau)/(P1_c_f - P1_n_f)
             
@@ -128,12 +131,20 @@ class calibration_model_bySex:
             kappa_dist_n_m = kappa_dist_m
             kappa_dist_c_f = kappa_dist_f
             kappa_dist_n_f = kappa_dist_f
-        
-            #elasticity_c = (w1_c)/(w1_c+tau-kappa_lb_c)
-            #elasticity_n = (w1_n)/(w1_n+tau-kappa_lb_n)
+                    
+            elasticity_c_m = kappa_dist_c_m**(-1) * (w1_c_m / P1_c_m)
+            elasticity_c_f = kappa_dist_c_f**(-1) * (w1_c_f / P1_c_f)
+            elasticity_n_m = kappa_dist_n_m**(-1) * (w1_n_m / P1_n_m)
+            elasticity_n_f = kappa_dist_n_f**(-1) * (w1_n_f / P1_n_f)
             
-        # else:
-        #     Calulcate kappa_dist_g_s
+        else:
+            elasticity_c_m, elasticity_c_f, \
+            elasticity_n_m, elasticity_n_f = elasticities
+            
+            kappa_dist_c_m = elasticity_c_m**(-1) * (w1_c_m / P1_c_m)
+            kappa_dist_c_f = elasticity_c_f**(-1) * (w1_c_f / P1_c_f)
+            kappa_dist_n_m = elasticity_n_m**(-1) * (w1_n_m / P1_n_m)
+            kappa_dist_n_f = elasticity_n_f**(-1) * (w1_n_f / P1_n_f)
     
         # Solve for MPL:
         MPL1_c_m = X_c_m*lambda_c*L1_c**(rho-1)*(lambda_n*L1_n**rho+lambda_c*L1_c**rho)**(1/rho-1)
@@ -164,7 +175,7 @@ class calibration_model_bySex:
         employment1_c_f = L1_c_f * pop_count 
         employment1_n_f = L1_n_f * pop_count
 
-    # Solve for epop ratio in Head Tax Eq
+        # Solve for epop ratio in Head Tax Eq
         epop_ratio1 = (P1_c_m*share_pop_c_m + P1_n_m*share_pop_n_m \
                        + P1_c_f*share_pop_c_f + P1_n_f*share_pop_n_f)
             
@@ -190,10 +201,10 @@ class calibration_model_bySex:
                 *(lambda_n*L2_n**(rho)+lambda_c*L2_c**(rho))**(1/rho-1))/(1+t)
             
             #Labor Supply
-            eq3_m = L2_n_m - (L1_n_m + share_pop_n_m*(w2_n_m-w1_n_m)/(kappa_dist_m))
-            eq3_f = L2_n_f - (L1_n_f + share_pop_n_f*(w2_n_f-w1_n_f)/(kappa_dist_f))
-            eq4_m = L2_c_m - (L1_c_m + share_pop_c_m*(w2_c_m-w1_c_m)/(kappa_dist_m))
-            eq4_f = L2_c_f - (L1_c_f + share_pop_c_f*(w2_c_f-w1_c_f)/(kappa_dist_f))
+            eq3_m = L2_n_m - (L1_n_m + share_pop_n_m*(w2_n_m-w1_n_m)/(kappa_dist_n_m))
+            eq3_f = L2_n_f - (L1_n_f + share_pop_n_f*(w2_n_f-w1_n_f)/(kappa_dist_n_f))
+            eq4_m = L2_c_m - (L1_c_m + share_pop_c_m*(w2_c_m-w1_c_m)/(kappa_dist_c_m))
+            eq4_f = L2_c_f - (L1_c_f + share_pop_c_f*(w2_c_f-w1_c_f)/(kappa_dist_c_f))
             
             #Balanced budget
             w_tilde = (L2_c_m * w2_c_m + L2_n_m * w2_n_m \
@@ -208,10 +219,10 @@ class calibration_model_bySex:
         w2_c_m_guess = MPL1_c_m/(1+t_guess)
         w2_n_f_guess = MPL1_n_f/(1+t_guess)
         w2_c_f_guess = MPL1_c_f/(1+t_guess)
-        L2_n_m_guess = L1_n_m + share_pop_n_m*(w2_n_m_guess-w1_n_m)/(kappa_dist_m)
-        L2_c_m_guess = L1_c_m + share_pop_c_m*(w2_c_m_guess-w1_c_m)/(kappa_dist_m)
-        L2_n_f_guess = L1_n_f + share_pop_n_f*(w2_n_f_guess-w1_n_f)/(kappa_dist_f)
-        L2_c_f_guess = L1_c_f + share_pop_c_f*(w2_c_f_guess-w1_c_f)/(kappa_dist_f)
+        L2_n_m_guess = L1_n_m + share_pop_n_m*(w2_n_m_guess-w1_n_m)/(kappa_dist_n_m)
+        L2_c_m_guess = L1_c_m + share_pop_c_m*(w2_c_m_guess-w1_c_m)/(kappa_dist_c_m)
+        L2_n_f_guess = L1_n_f + share_pop_n_f*(w2_n_f_guess-w1_n_f)/(kappa_dist_n_f)
+        L2_c_f_guess = L1_c_f + share_pop_c_f*(w2_c_f_guess-w1_c_f)/(kappa_dist_c_f)
        
         #Solve 
         w2_n_m, w2_c_m,w2_n_f, w2_c_f,\
@@ -227,16 +238,19 @@ class calibration_model_bySex:
         L2_n = X_n_m * L2_n_m + X_n_f * L2_n_f 
         
         #Solve for employment rate
-        P2_c_m = P1_c_m + (w2_c_m-w1_c_m) / kappa_dist_m
-        P2_c_f = P1_c_f + (w2_c_f-w1_c_f) / kappa_dist_f
-        P2_n_m = P1_n_m + (w2_n_m-w1_n_m) / kappa_dist_m
-        P2_n_f = P1_n_f + (w2_n_f-w1_n_f) / kappa_dist_f
+        P2_c_m = P1_c_m + (w2_c_m-w1_c_m) / kappa_dist_c_m
+        P2_c_f = P1_c_f + (w2_c_f-w1_c_f) / kappa_dist_c_f
+        P2_n_m = P1_n_m + (w2_n_m-w1_n_m) / kappa_dist_n_m
+        P2_n_f = P1_n_f + (w2_n_f-w1_n_f) / kappa_dist_n_f
         
-        #Solve for share of workers and avg. productivity
+        #Solve for share of workers
         share_workers2_c_m = L2_c_m / (L2_c_m + L2_n_m + L2_c_f + L2_n_f)
-        share_workers2_n_m =  L2_n_m / (L2_c_m + L2_n_m + L2_c_f + L2_n_f)
         share_workers2_c_f = L2_c_f / (L2_c_m + L2_n_m + L2_c_f + L2_n_f)
+        share_workers2_c = share_workers2_c_m + share_workers2_c_f
+        
+        share_workers2_n_m =  L2_n_m / (L2_c_m + L2_n_m + L2_c_f + L2_n_f)
         share_workers2_n_f =  L2_n_f / (L2_c_m + L2_n_m + L2_c_f + L2_n_f)
+        share_workers2_n = share_workers2_n_m + share_workers2_n_f
         
         # Solve for MPL in Payroll Tax Eq
         MPL2_c_m = X_c_m*lambda_c*L2_c**(rho-1)*(lambda_n*L2_n**rho+lambda_c*L2_c**rho)**(1/rho-1)
@@ -244,7 +258,7 @@ class calibration_model_bySex:
         MPL2_c_f = X_c_f*lambda_c*L2_c**(rho-1)*(lambda_n*L2_n**rho+lambda_c*L2_c**rho)**(1/rho-1)
         MPL2_n_f = X_n_f*lambda_n*L2_n**(rho-1)*(lambda_n*L2_n**rho+lambda_c*L2_c**rho)**(1/rho-1)
         
-        # Solve for average productivity in Head Tax Eq
+        # Solve for average productivity in Payroll Tax Eq
         avg_MPL2 = share_workers2_c_m * MPL2_c_m + share_workers2_n_m * MPL2_n_m \
                     + share_workers2_c_f * MPL2_c_f + share_workers2_n_f * MPL2_n_f
             
@@ -271,7 +285,25 @@ class calibration_model_bySex:
         epop_ratio2 = (P2_c_m*share_pop_c_m + P2_n_m*share_pop_n_m \
                        + P2_c_f*share_pop_c_f + P2_n_f*share_pop_n_f)
         
-                  
+    
+        ## Calculating Aggregates ##
+        # Wages
+        avg_w1_c = (w1_c_m*share_workers1_c_m +w1_c_f*share_workers1_c_f)/share_workers1_c
+        avg_w1_n = (w1_n_m*share_workers1_n_m +w1_n_f*share_workers1_n_f)/share_workers1_n
+        avg_w2_c = (w2_c_m*share_workers2_c_m +w2_c_f*share_workers2_c_f)/share_workers2_c
+        avg_w2_n = (w2_n_m*share_workers2_n_m +w2_n_f*share_workers2_n_f)/share_workers2_n
+        # Employment Rate
+        avg_P1_c = (P1_c_m*share_pop_c_m +P1_c_f*share_pop_c_f)/share_pop_c
+        avg_P1_n = (P1_n_m*share_pop_n_m +P1_n_f*share_pop_n_f)/share_pop_n
+        avg_P2_c = (P2_c_m*share_pop_c_m +P2_c_f*share_pop_c_f)/share_pop_c
+        avg_P2_n = (P2_n_m*share_pop_n_m +P2_n_f*share_pop_n_f)/share_pop_n
+        # Wage Bill
+        share_wage_bill1_c = share_wage_bill1_c_m + share_wage_bill1_c_f 
+        share_wage_bill1_n = share_wage_bill1_n_m + share_wage_bill1_n_f 
+        share_wage_bill2_c = share_wage_bill2_c_m + share_wage_bill2_c_f
+        share_wage_bill2_n = share_wage_bill2_n_m + share_wage_bill2_n_f
+        
+        
         ### Store variables as attributes of object ###
         general_params = ['tau', 'pop_count', 't']
         for var in general_params:
@@ -282,27 +314,33 @@ class calibration_model_bySex:
             for _type in ['_c','_n']:        
                     exec("self."+var+_type+'='+var+_type)
                     
-        general_params_w_subtype = ['X', 'share_pop', 'kappa_dist']
+        general_params_w_subtype = ['X', 'share_pop', 'kappa_dist', 'elasticity']
         for var in general_params_w_subtype:
             for _type in ['_c','_n']:
                 for _sex in ['_m','_f']:
                     exec("self."+var+_type+_sex+'='+var+_type+_sex)
         
-        eq_values_w_type = ['w', 'P', 'L', 'MPL', 'share_workers', 'share_wage_bill', 'employment']
+        eq_values = ['avg_MPL', 'epop_ratio']
+        for var in eq_values:
+            for eq_num in ['1', '2']:
+                exec("self."+var+eq_num+'='+var+eq_num)
+                
+        eq_values_w_type = ['avg_w', 'avg_P', 'share_wage_bill']
         for var in eq_values_w_type:
+            for _type in ['_c','_n']:       
+                for eq_num in ['1', '2']:
+                    exec("self."+var+eq_num+_type+'='+var+eq_num+_type)
+        
+        eq_values_w_subtype = ['w', 'P', 'L', 'MPL', 'share_workers', 'share_wage_bill', 'employment']
+        for var in eq_values_w_subtype:
             for eq_num in ['1', '2']:
                 for _type in ['_c','_n']:
                     for _sex in ['_m','_f']:        
                         exec("self."+var+eq_num+_type+_sex+'='+var+eq_num+_type+_sex)
         
-        eq_values_wo_type = ['avg_MPL', 'epop_ratio']
-        for var in eq_values_wo_type:
-            for eq_num in ['1', '2']:
-                exec("self."+var+eq_num+'='+var+eq_num)
 
 #%%  Create Instance #%%
-
-'''              
+'''
 # Importing Data
 os.chdir(data_folder)
 df_observed = pd.read_csv('observed_data_bySex.csv', index_col=0)
@@ -321,7 +359,7 @@ e_c_baseline, e_n_baseline = elasticity_baseline[0], elasticity_baseline[1]
 model = calibration_model_bySex(alpha_diff=0,
                     rho=rho_baseline,
                     tau=df_observed.loc[year, tau_baseline],
-                    elasticity_c=e_c_baseline, elasticity_n=e_n_baseline,
+                    elasticities=[0.15,0.25,0.4,0.4],
                     w1_c_m=df_observed.loc[year, 'wage1_c_m'], 
                     w1_n_m=df_observed.loc[year, 'wage1_n_m'],
                     w1_c_f=df_observed.loc[year, 'wage1_c_f'], 
@@ -340,9 +378,7 @@ model = calibration_model_bySex(alpha_diff=0,
 
 #Make sure there are no NANs in model before calibration
 # Remove elasticities if specified to be common
-if model.elasticity_c == model.elasticity_n == 'common': 
-    check = set(list(vars(model).keys())) - set(['elasticity_c', 'elasticity_n'])
-else: check = vars(model).keys()
+check = set(list(vars(model).keys())) - set(['elasticities'])
 #And now check
 if any(np.isnan([vars(model)[x] for x in check])):
     print("NAN value entered into calibration model for:")
