0237aa0bd61f366a6bbb07f94c6932e5bc279f57
Casey McQuillan <mcquillan.casey@gmail.com>
2022-07-14 11:36:17 -0400

    Update fzz_EqComparison_bySex.py

--- fzz_EqComparison_bySex.py
+++ fzz_EqComparison_bySex.py
@@ -42,9 +42,15 @@ tau_string = '\\underline{Fixed Per Worker Cost, $\\tau$:} \n \t'
 payroll_tax_string = '\\underline{Payroll Tax Rate, $t$:} \n \t'
 delta_w_C_string = '\\ \\ Change in College Wage, $\Delta(w_C)$ \n \t'
 delta_w_N_string = '\\ \\ Change in Non-college Wage, $\Delta(w_N)$ \n \t'
+pct_chg_cwp_string = '\\ \\ Pct. Change in College Wage Premium, $\\%\\Delta(w_C/w_N - 1)$ \n \t'
 delta_P_c_string = '\\ \\ Change in College Employment Rate, $\Delta(P_C)$ \n \t'
 delta_P_n_string = '\\ \\ Change in Non-college Employment Rate, $\Delta(P_N)$ \n \t'
-    
+delta_employment_string = 'Change in Total Employment, $\Delta(L)$ \n \t'
+delta_employment_C_string = '\\ \\ \\small Change in College Employment, $\Delta(L_C)$ \n \t'
+delta_employment_N_string = '\\ \\ \\small Change in Non-college Employment, $\Delta(L_N)$ \n \t'
+delta_cwb_string = 'Change in College Share of Wage Bill, $\Delta(\\frac{w_C L_C}{w_N L_N+w_C L_C})$: \n \t'
+
+
 #Define Model
 model = calibration_model_bySex(alpha_diff=0,
             rho=rho_baseline,
@@ -84,17 +90,37 @@ model.calibrate()
 #Calculate Changes
 chg_w_C = model.avg_w2_c-model.avg_w1_c
 chg_w_N = model.avg_w2_n-model.avg_w1_n
+chg_cwp = 100*((model.avg_w2_c/model.avg_w2_n)-(model.avg_w1_c/model.avg_w1_n))/(model.avg_w1_c/model.avg_w1_n -1)
+
 chg_P_C=100*(model.avg_P2_c-model.avg_P1_c)
 chg_P_N=100*(model.avg_P2_n-model.avg_P1_n)
-    
+chg_employment_C = ((model.employment2_c_m+model.employment2_c_f)-(model.employment1_c_m+model.employment1_c_f))
+chg_employment_N = ((model.employment2_n_m+model.employment2_n_f)-(model.employment1_n_m+model.employment1_n_f))
+chg_employment = chg_employment_C+chg_employment_N
+
+chg_cwb = 100*((((model.L2_c_m+model.L2_c_f)*model.avg_w2_c)/\
+                ((model.L2_c_m+model.L2_c_f)*model.avg_w2_c \
+                 + (model.L2_n_m+model.L2_n_f)*model.avg_w2_n))\
+               -(((model.L1_c_m+model.L1_c_f)*model.avg_w1_c)/\
+                 ((model.L1_c_m+model.L1_c_f)*model.avg_w1_c \
+                  + (model.L1_n_m+model.L1_n_f)*model.avg_w1_n)))
+
 #Add to strings
 ampersand = '&'
 tau_string = tau_string + ampersand + f' \${model.tau:,.0f} '
 payroll_tax_string = payroll_tax_string + ampersand + f' {100*((model.t)):,.2f}\\% '
+
 delta_w_C_string = delta_w_C_string + ampersand + f' \${chg_w_C:,.0f} '
 delta_w_N_string = delta_w_N_string + ampersand + f' \${chg_w_N:,.0f} '
+pct_chg_cwp_string = pct_chg_cwp_string + ampersand + f' {chg_cwp:,.2f}\\% '
+
 delta_P_c_string = delta_P_c_string + ampersand + f' {chg_P_C:,.2f} pp '
 delta_P_n_string = delta_P_n_string + ampersand + f' {chg_P_N:,.2f} pp '
+delta_employment_string = delta_employment_string + ampersand + f' {chg_employment:,.0f} '
+delta_employment_C_string = delta_employment_C_string + ampersand + f' {chg_employment_C:,.0f} '
+delta_employment_N_string = delta_employment_N_string + ampersand + f' {chg_employment_N:,.0f} '
+
+delta_cwb_string = delta_cwb_string + ampersand + f' {chg_cwb:,.2f} pp'
 
 
 ### Add Values to strings for each sex
@@ -108,15 +134,26 @@ for _sex in ['_m', '_f']:
     exec(f'chg_w_N = model.w2_n{_sex}-model.w1_n{_sex}')
     exec(f'chg_P_C=100*(model.P2_c{_sex}-model.P1_c{_sex})')
     exec(f'chg_P_N=100*(model.P2_n{_sex}-model.P1_n{_sex})')
+    exec(f'chg_employment_C=model.employment2_c{_sex}-model.employment1_c{_sex}')
+    exec(f'chg_employment_N=model.employment2_n{_sex}-model.employment1_n{_sex}')
+    chg_employment = chg_employment_C+chg_employment_N
+
         
     #Add to strings
     tau_string = tau_string + ampersand + f' -  '
     payroll_tax_string = payroll_tax_string + ampersand + f' - '
+    
     delta_w_C_string = delta_w_C_string + ampersand + f' \${chg_w_C:,.0f} '
     delta_w_N_string = delta_w_N_string + ampersand + f' \${chg_w_N:,.0f} '
+    pct_chg_cwp_string = pct_chg_cwp_string + ampersand + f' - '
+    
     delta_P_c_string = delta_P_c_string + ampersand + f' {chg_P_C:,.2f} pp '
     delta_P_n_string = delta_P_n_string + ampersand + f' {chg_P_N:,.2f} pp '
-
+    delta_employment_string = delta_employment_string + ampersand + f' {chg_employment:,.0f} '
+    delta_employment_C_string = delta_employment_C_string + ampersand + f' {chg_employment_C:,.0f} '
+    delta_employment_N_string = delta_employment_N_string + ampersand + f' {chg_employment_N:,.0f} '
+    
+    delta_cwb_string = delta_cwb_string + ampersand + f' - '
 
 #Output Table
 header = ['\\begin{tabular}{lcccccc}', '\n',
@@ -133,17 +170,17 @@ table_values=[tau_string, ' \\\\\n',
                 '\\underline{Wages:}', ' \\\\\n',
                 delta_w_C_string, ' \\\\\n',
                 delta_w_N_string, ' \\\\\n',
-                # pct_chg_cwp_string, ' \\\\\n',
+                pct_chg_cwp_string, ' \\\\\n',
                 '\\\\\n',
                 '\\underline{Employment Rate:}', ' \\\\\n',
                 delta_P_c_string, ' \\\\\n',
                 delta_P_n_string, ' \\\\\n',
-                # delta_employment_string, ' \\\\\n',
-                # delta_employment_C_string, ' \\\\\n',
-                # delta_employment_N_string, ' \\\\\n',
-                '\\\\\n']
-                # '\\underline{Wage Bill:}', ' \\\\\n',
-                # delta_cwb_string,' \\\\\n']
+                delta_employment_string, ' \\\\\n',
+                delta_employment_C_string, ' \\\\\n',
+                delta_employment_N_string, ' \\\\\n',
+                '\\\\\n',
+                '\\underline{Wage Bill:}', ' \\\\\n',
+                delta_cwb_string,' \\\\\n']
 
 closer = ['\\bottomrule','\n', '\end{tabular}']
 
