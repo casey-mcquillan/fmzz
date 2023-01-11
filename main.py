ac133b527a1461561809c7db0edc7042abdc9423
Casey McQuillan <mcquillan.casey@gmail.com>
2023-01-10 22:20:14 -0500

    Executing scripts from main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
+"""s
 Created on Mon Jan  9 23:31:36 2023
 
 @author: caseymcquillan
@@ -12,15 +12,47 @@ import numpy as np
 
 ### Set working directory and folders
 main_folder = "/Users/caseymcquillan/Desktop/Research/FZZ"
+code_folder = main_folder+"/code"
+data_folder = main_folder+"/data"
+    
+    
+#%%  Clean Data  for Main Analysis #%%  
 
 
-#%%  Clean Data #%%  
+# Clean ASEC Data
 
 
-#%%  Main Tables #%%  
+
+#%%  Generate Main Tables #%% 
+
+# Generate Table 1:  
+os.chdir(code_folder)
+exec(open("summary_stats.py").read())
+
+# Generate Table XX:  
+os.chdir(code_folder)
+exec(open("EquilibriumComparison.py").read())
+
+# Generate Table XX:  
+os.chdir(code_folder)
+exec(open("EquilibriumComparison_Robustness.py").read())
+
+# Generate Table XX:  
+os.chdir(code_folder)
+exec(open("CounterfactualGrowth.py").read())
+
+# Generate Table XX:  
+os.chdir(code_folder)
+exec(open("CounterfactualGrowth_Robustness.py").read())
+
+# Generate Table XX:  
+os.chdir(code_folder)
+exec(open("CostCounterfactual.py").read())
 
 
 #%%  Appendix: Alternative College Definition #%%  
+
+
 
 
 #%%  Appendix: Decomposing Effects by Sex #%%  
